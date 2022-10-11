dce8eeec00c4577e4532932a7a16677f5f998b3e
sleek1004 <arojojoyeo@yahoo.com>
2022-10-11 23:42:57 +0100

    creating react app

---
+++ src/component/Navbar.js
@@ -0,0 +1,12 @@
+import React  from "react"
+
+function Navbar(){
+    return(
+      <div>
+        <img src="../images/react-icon-small.png"/>
+       <h3>ReactFacts</h3>
+     <h4>React Course - Project 1 </h4>
+      </div>
+    )
+  }
+  export default Navbar;
